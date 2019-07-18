15f9f02098f5a52f8b06dedc3322087397bbac54
Timothy Grindall <tim.grindall@gmail.com>
2019-07-18 15:41:24 -0700

    twitter-clone-apollo-server

---
+++ models/TweetModel.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose")
+
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/twitter", {
+	useNewUrlParser: true
+})
+
+const Schema = mongoose.Schema
+
+const tweetSchema = new Schema({
+	tweet: String,
+	author: String,
+})
+
+const TweetModel = mongoose.model("Tweet", tweetSchema)
+
+// had to use "module.exports =" instead of newer "export default"
+
+module.exports = {
+	getTweets: () => TweetModel.find().sort({ _id: -1}),
+	getTweet: _id => TweetModel.findOne({ _id }),
+	createTweet: args => TweetModel(args).save(),
+	deleteTweet: args => {
+		const { _id } = args
+
+		TweetModel.remove({ _id }, error => {
+			if (error) {
+				console.log("Error Removing: ", error)
+			}
+		})
+
+		return args
+	},
+	updateTweet: args => {
+		const { _id, tweet } = args
+
+		TweetModel.update(
+			{ _id },
+			{
+				$set: { tweet }
+			},
+			{ upsert: true },
+			error => {
+				if (error) {
+					console.log("Error Updating: ", error)
+				}
+			}
+		)
+
+		args.author = "User123"
+
+		return args
+	}
+}
